370c76ef39ba3aa14e78e80eb941a2068d4df652
DaisyChubb <homm0004@algonquincollege.com>
2016-11-14 13:46:04 -0500

    Created Login Screen & Menu Should be good to branch off into our own branches now

--- app/src/main/java/com/algonquin/cst2335/smarthomecontroller/RoomListActivity.java
+++ app/src/main/java/com/algonquin/cst2335/smarthomecontroller/RoomListActivity.java
@@ -1,141 +1,52 @@
 package com.algonquin.cst2335.smarthomecontroller;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 
-import com.algonquin.cst2335.smarthomecontroller.home.DummyContent;
-
-import java.util.List;
-
-/**
- * An activity representing a list of Rooms. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link RoomDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- */
 public class RoomListActivity extends AppCompatActivity {
-
-    /** mTwoPane instance variable
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
+    ArrayList<String> rooms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_list);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle(getTitle());
+        ListView roomsList = (ListView) findViewById(R.id.roomListView);
+        rooms = new ArrayList<>();
+        rooms.add(getResources().getString(R.string.roomHome));
+        rooms.add(getResources().getString(R.string.roomLiving));
+        rooms.add(getResources().getString(R.string.roomKitchen));
+        rooms.add(getResources().getString(R.string.roomAuto));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        roomsList.setAdapter(new ArrayAdapter<>(
+                this, R.layout.list_item, rooms
+        ));
+
+        roomsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> adapterView, View itemClicked, int position, long id) {
+                TextView textView = (TextView) itemClicked;
+                String selectedItem = textView.getText().toString();
+                //if strTest = name of menu item string, launch that activity
+                if (selectedItem.equalsIgnoreCase(getResources().getString(R.string.roomHome))){
+                    startActivity(new Intent(RoomListActivity.this, HomeListActivity.class));
+                } else if (selectedItem.equalsIgnoreCase(getResources().getString(R.string.roomLiving))){
+                    startActivity(new Intent(RoomListActivity.this, LivingRoomListActivity.class));
+                } else if (selectedItem.equalsIgnoreCase(getResources().getString(R.string.roomKitchen))){
+                    startActivity(new Intent(RoomListActivity.this, KitchenListActivity.class));
+                } else if (selectedItem.equalsIgnoreCase(getResources().getString(R.string.roomAuto))){
+                    startActivity(new Intent(RoomListActivity.this, AutomobileListActivity.class));
+                }
             }
         });
-
-        View recyclerView = findViewById(R.id.room_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
-
-        if (findViewById(R.id.room_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp). (Tablets)
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
-    }
-
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
-    }
-
-    public class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-        private final List<DummyContent.DummyItem> mValues;
-
-        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
-            mValues = items;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.room_list_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mItem = mValues.get(position);
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
-
-            holder.mView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mTwoPane) {
-                        Bundle arguments = new Bundle();
-                        arguments.putString(RoomDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-                        RoomDetailFragment fragment = new RoomDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.room_detail_container, fragment)
-                                .commit();
-                    } else {
-                        Context context = v.getContext();
-                        Intent intent = new Intent(context, RoomDetailActivity.class);
-                        intent.putExtra(RoomDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-
-                        context.startActivity(intent);
-                    }
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public final View mView;
-            public final TextView mIdView;
-            public final TextView mContentView;
-            public DummyContent.DummyItem mItem;
-
-            public ViewHolder(View view) {
-                super(view);
-                mView = view;
-                mIdView = (TextView) view.findViewById(R.id.id);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
-
-            @Override
-            public String toString() {
-                return super.toString() + " '" + mContentView.getText() + "'";
-            }
-        }
     }
 }
