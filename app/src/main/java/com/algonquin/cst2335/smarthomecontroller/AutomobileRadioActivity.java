d589dd0e16838978a1371c196f34d1ce12868957
James Jarrett <the_jj_man@hotmail.com>
2016-12-08 11:15:42 -0500

    Automobile Activity V0.3.2
    Activity editing

--- app/src/main/java/com/algonquin/cst2335/smarthomecontroller/AutomobileRadioActivity.java
+++ app/src/main/java/com/algonquin/cst2335/smarthomecontroller/AutomobileRadioActivity.java
@@ -1,7 +1,15 @@
 package com.algonquin.cst2335.smarthomecontroller;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AutomobileRadioActivity extends AppCompatActivity {
 
@@ -10,6 +18,42 @@ public class AutomobileRadioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_automobile_radio);
 
+        addRadio = (Button) findViewById(R.id.addRadio);
+        addRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(AutomobileRadioActivity.this);
 
+                LayoutInflater inflater = AutomobileRadioActivity.this.getLayoutInflater();
+                final View view = inflater.inflate(R.layout.add_radio_dialog, null);
+
+                builder.setView(view)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id)
+                            {
+                                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                                        R.array.fmRadio, android.R.layout.simple_spinner_item);
+                                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                                Spinner radioSpinner = (Spinner) findViewById(R.id.radioSpinner);
+                                radioSpinner.setAdapter(adapter);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener()
+                        {
+                            public void onClick(DialogInterface dialog, int id)
+                            {
+                                //User clicked cancel button
+                            }
+                        })
+                        .create()
+                        .show();
+
+            }
+        });
     }
+
+    private Button addRadio;
 }
