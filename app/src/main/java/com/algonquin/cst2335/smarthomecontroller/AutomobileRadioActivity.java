3d1c77419096fbb0da215ab852a91808d241f87d
James Jarrett <the_jj_man@hotmail.com>
2016-12-08 11:51:48 -0500

    Automobile Activity V0.4.0
    Readyish for demo

--- app/src/main/java/com/algonquin/cst2335/smarthomecontroller/AutomobileRadioActivity.java
+++ app/src/main/java/com/algonquin/cst2335/smarthomecontroller/AutomobileRadioActivity.java
@@ -1,11 +1,13 @@
 package com.algonquin.cst2335.smarthomecontroller;
 
 import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,18 +29,35 @@ public class AutomobileRadioActivity extends AppCompatActivity {
                 LayoutInflater inflater = AutomobileRadioActivity.this.getLayoutInflater();
                 final View view = inflater.inflate(R.layout.add_radio_dialog, null);
 
+                final Spinner radioSpinner = (Spinner) view.findViewById(R.id.radioSpinner);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
+                        R.array.fmRadio, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                radioSpinner.setAdapter(adapter);
+                radioSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        stationAdded = parent.getItemAtPosition(position).toString();
+                        radioAdded = Snackbar.make(findViewById(R.id.activity_automobile_radio), "Added station " + stationAdded, Snackbar.LENGTH_LONG)
+                                .setAction("Action", null);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        stationAdded = "none";
+                        radioAdded = Snackbar.make(findViewById(R.id.activity_automobile_radio), "Added station " + stationAdded, Snackbar.LENGTH_LONG)
+                                .setAction("Action", null);
+                    }
+                });
+
                 builder.setView(view)
                         .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
                         {
                             @Override
                             public void onClick(DialogInterface dialog, int id)
                             {
-                                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(),
-                                        R.array.fmRadio, android.R.layout.simple_spinner_item);
-                                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-                                Spinner radioSpinner = (Spinner) findViewById(R.id.radioSpinner);
-                                radioSpinner.setAdapter(adapter);
+                                radioAdded.show();
                             }
                         })
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener()
@@ -56,4 +75,6 @@ public class AutomobileRadioActivity extends AppCompatActivity {
     }
 
     private Button addRadio;
+    private Snackbar radioAdded;
+    private String stationAdded;
 }
