83c6e883c61f98d50aa18bac5862ed37b27a04bf
DaisyChubb <homm0004@algonquincollege.com>
2016-12-08 13:34:46 -0500

    Adding Toolbar to Home activity woohoo!

--- app/src/main/java/com/algonquin/cst2335/smarthomecontroller/HomeSubMenu.java
+++ app/src/main/java/com/algonquin/cst2335/smarthomecontroller/HomeSubMenu.java
@@ -1,11 +1,16 @@
 package com.algonquin.cst2335.smarthomecontroller;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +32,11 @@ public class HomeSubMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_sub_menu);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+       // toolbar.setTitle(getTitle());
+
         HomeSubMenuLstFragment lstFragment = (HomeSubMenuLstFragment)getSupportFragmentManager().findFragmentByTag("lstfragment");
         if(lstFragment==null){
             lstFragment=new HomeSubMenuLstFragment();
@@ -34,6 +44,55 @@ public class HomeSubMenu extends AppCompatActivity {
             transaction.add(android.R.id.content,lstFragment,"lstfragment");
             transaction.commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        //if it's help, show the help screen!
+        if (menuItem.getItemId() ==  R.id.action_help) {
+            final android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(this);
+
+            builder.setTitle("Automobile Activity");
+            builder.setMessage("Activity made by James Jarrett. Select an option to view and change " +
+                    "relevant settings.");
+            builder .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            final android.app.AlertDialog alert = builder.create();
+            alert.show();
+
+        } else {
+            Intent intent = new Intent();
+            switch (menuItem.getItemId()) {
+                case R.id.action_home:
+                    intent = new Intent(this, HomeSubMenu.class);
+                    startActivity(intent);
+                    break;
+                case R.id.action_sofa:
+                    intent = new Intent(this, LRHome.class);
+                    startActivity(intent);
+                    break;
+                case R.id.action_fridge:
+                    intent = new Intent(this, KitchenListActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.action_car:
+                    intent = new Intent(this, AutomobileListActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+
+        }
+        return true;
     }
 
 
