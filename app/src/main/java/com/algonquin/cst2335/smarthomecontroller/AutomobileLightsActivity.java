ef98eeb3e23005695cf233a3fd80cb22cbad3581
James Jarrett <the_jj_man@hotmail.com>
2016-12-08 10:09:14 -0500

    Automobile Activity V0.3.0
    Added all necessary activities (incomplete)

---
+++ app/src/main/java/com/algonquin/cst2335/smarthomecontroller/AutomobileLightsActivity.java
@@ -0,0 +1,13 @@
+package com.algonquin.cst2335.smarthomecontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AutomobileLightsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_automobile_lights);
+    }
+}
